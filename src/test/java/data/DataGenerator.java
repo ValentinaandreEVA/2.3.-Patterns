bec3f44c75380b50d57809669d927db309731077
Валентина Андреева <andre8159@gmail.com>
2023-12-13 20:23:31 +0300

    initial commit

--- src/test/java/data/DataGenerator.java
+++ src/test/java/data/DataGenerator.java
@@ -19,7 +19,7 @@ public class DataGenerator {
     }
 
     public static String generateCity() {
-        var cities = new String[] {"Астрахань", "Санкт-Петербург", "Рязань", "Москва", "Пермь", "Кострома"};
+        var cities = new String[] {"Уфа", "Астрахань", "Санкт-Петербург", "Рязань", "Москва", "Пермь", "Кострома"};
         return cities[new Random().nextInt(cities.length)];
     }
 
